519457042f09e47bc8b6e5bd0135119f8c59e973
pmita <panos.mita2@outlook.com>
2022-04-19 12:15:57 +0100

    fetching data from jsonplaceholder

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Articles = () => {
+    //STATE & VARIABLES
+    const [articles, setArticles] = useState(null);
+
+    //useEFFECT
+    useEffect(() => {
+        setTimeout(async () => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await res.json();
+            setArticles(data);
+        }, 500)
+    }, []);
     return(
         <div className="articles">
             <h2>Articles</h2>
+            {articles && articles.map((article) => (
+                <div className='article' key={article.id}>
+                    <h3>{article.title}</h3>
+                    <p>{article.body}</p>
+                </div>
+            ))}
+
+            {!articles && <div>Loading ...</div>}
         </div>
     );
 }
